db38f724c5ed305d7a41f8b5359fa93238efc571
nspp99b <chromepanthers@gmail.com>
2018-01-30 09:34:36 -0500

    defined components with state

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,21 @@
 // Bomb Component Code Goes Here
+import React from 'react';
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    const bomb_text = this.state.secondsLeft > 0 ? `${this.state.secondsLeft} seconds left before I go boom!` : 'Boom!'
+    return (
+      <div>{bomb_text}
+      </div>
+    )
+  }
+}
+
+export default Bomb;
